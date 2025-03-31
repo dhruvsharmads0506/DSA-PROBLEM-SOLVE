50da714291eee5b6231e17a91f7f94dfaba06f7c
Dhruv Sharma <dhruvsharmads0506@gmail.com>
2025-03-31 14:04:07 +0530

    Pivot Element in Rotated Sorted Array

---
+++ PIvot.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int Pivotelement(int arr[], int size) {
+int s=0;
+int e=size-1;
+int mid = s+(e-s)/2;
+while(s<e) {
+if(arr[mid]>=arr[0]) {
+    s=mid+1;
+}
+else 
+{
+    e=mid;
+}
+mid = s+(e-s)/2;
+}
+return s;
+
+}
+
+int main() {
+int arr[]={7,9,1,2,3};
+int pivot = Pivotelement(arr, 5);
+cout<<pivot;
+return 0;
+}
